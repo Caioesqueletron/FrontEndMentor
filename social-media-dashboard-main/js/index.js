89d124c2ad2a7f62f5a7ae505bd8696dfa03f276
caioesqueletron <caiocaio620@gmail.com>
2021-02-21 16:51:08 -0300

    Challange finished

--- social-media-dashboard-main/js/index.js
+++ social-media-dashboard-main/js/index.js
@@ -1,5 +1,4 @@
 let btnToggle = document.querySelector('.btn-Toggle');
-
 btnToggle.addEventListener('click',()=>{
-   
+    (btnToggle.checked) ? document.body.classList.add('dark') : document.body.classList.remove('dark')
 })
